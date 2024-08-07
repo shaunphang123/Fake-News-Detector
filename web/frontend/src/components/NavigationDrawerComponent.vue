ad301aa27dd31355f2284537ea74a777eb366c28
shaunphang123 <shaun.phang@hotmail.com>
2024-08-07 22:06:41 +0800

    Added dill to requirements

--- web/frontend/src/components/NavigationDrawerComponent.vue
+++ web/frontend/src/components/NavigationDrawerComponent.vue
@@ -13,7 +13,7 @@ export default {
         rail
     >
         <v-list density="compact" nav>
-            <v-tooltip v-for="pastPrediction in pastPredictions" :text="`${(parseFloat(pastPrediction.prediction_score)).toFixed(2)}% certainty that this is fake news`">
+            <v-tooltip v-for="pastPrediction in pastPredictions" :text="`${(parseFloat(pastPrediction.prediction_score * 100)).toFixed(2)}% certainty that this is fake news`">
                 <template v-slot:activator="{ props }">
                     <v-list-item :key="pastPrediction.article" prepend-icon="mdi-book-alphabet" value="starred" v-bind="props">
                         <v-list-item-title>{{ pastPrediction.article }}</v-list-item-title>
